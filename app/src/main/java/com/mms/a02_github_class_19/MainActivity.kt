617b1f128810c59e779f8a360145137193e171a4
Shahriar989 <mmalshahriar@gmail.com>
2022-12-21 00:55:36 +0600

    initial

---
+++ app/src/main/java/com/mms/a02_github_class_19/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.mms.a02_github_class_19
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
